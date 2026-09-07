3b8e9ad6750d19ed75d57f9da9ef6dd4d099056e
jjmorg04 <jjmorg04@gmail.com>
2026-09-06 21:43:06 -0700

    More logic

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 
 import pygame
 
-player_data = [[' '] * 3] * 3
+player_data = [[0,0,0], [0,0,0], [0,0,0]]
 test_data = [[1,0,0], [0,1,0], [0,0,1]]
+
+pygame.display.set_caption("Tic Tac Toe!")
 
 print(player_data)
 print(test_data)
 pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 running = True
+
+p1_turn = True
+
+x_img = pygame.image.load('X.png').convert()
+o_img = pygame.image.load('O.png').convert()
 
 pt1 = pygame.Rect(0,0,200,200)
 pt2 = pygame.Rect(200,0,200,200)
@@ -22,48 +30,80 @@ pt7 = pygame.Rect(0,400,200,200)
 pt8 = pygame.Rect(200,400,200,200)
 pt9 = pygame.Rect(400,400,200,200)
 
+ctr = 1
+
+game_start = False
+
 while running:
 
-    ### Pre-game procedures
+    start_button = pygame.Rect(200,350,100,100)
+    game_start = True
 
-    mouse_pos = pygame.mouse.get_pos()
+    while game_start:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        ### Pre-game procedures
 
-    screen.fill("gray")
+        mouse_pos = pygame.mouse.get_pos()
 
-    # vertical lines
-    pygame.draw.line(screen, "black", (200, 0), (200, 600), width=12)
-    pygame.draw.line(screen, "black", (400, 0), (400, 600), width=12)
-    
-    # horizontal lines
-    pygame.draw.line(screen, "black", (0, 200), (600, 200), width=12)
-    pygame.draw.line(screen, "black", (0, 400), (600, 400), width=12)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    for i in range(len(test_data)):
-        for j in range(len(test_data)):
-            if test_data[i][j] == 1:
-                pygame.draw.rect(screen, "red", [i * 200, j * 200, 100, 100])
+        screen.fill("white")
 
-    pygame.display.flip()
+        # vertical lines
+        pygame.draw.line(screen, "black", (200, 0), (200, 600), width=12)
+        pygame.draw.line(screen, "black", (400, 0), (400, 600), width=12)
+        
+        # horizontal lines
+        pygame.draw.line(screen, "black", (0, 200), (600, 200), width=12)
+        pygame.draw.line(screen, "black", (0, 400), (600, 400), width=12)
+
+        for i in range(len(player_data)):
+            for j in range(len(player_data)):
+                if player_data[i][j] == 1:
+                    screen.blit(x_img, (j * 200, i * 200))
+                elif player_data[i][j] == 2:
+                    screen.blit(o_img, (j * 200, i * 200))
+                
+        # vertical lines
+        pygame.draw.line(screen, "black", (200, 0), (200, 600), width=12)
+        pygame.draw.line(screen, "black", (400, 0), (400, 600), width=12)
+        
+        # horizontal lines
+        pygame.draw.line(screen, "black", (0, 200), (600, 200), width=12)
+        pygame.draw.line(screen, "black", (0, 400), (600, 400), width=12)
+        pygame.draw.rect(screen, "red", (200, 350, 400, 100))
+                
+
+        pygame.display.flip()
+
+        if ctr % 2 == 0:
+            p1_turn = False
+        else:
+            p1_turn = True
+
+        ### Game start
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if p1_turn == True:
+                    if player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] == 1 or player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] == 2:
+                        continue
+                    player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] = 1
+                else:
+                    if player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] == 1 or player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] == 2:
+                        continue
+                    player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] = 2
+                ctr += 1
+                print(ctr)
+                print(player_data[0])
 
 
-    ### Game start
+        clock.tick(60)
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-            player_data[mouse_pos[1] // 200][mouse_pos[0] // 200] = 1
-
-        for i in range(len(test_data)):
-                for j in range(len(test_data)):
-                    if test_data[i][j] == 1:
-                        pygame.draw.rect(screen, "red", [i * 200, j * 200, 100, 100])
-        pygame.display.flip() 
-
-
-    clock.tick(60)
+        if player_data[0] == [1, 1, 1]:
+            game_start = False
 
 print(player_data)
 
